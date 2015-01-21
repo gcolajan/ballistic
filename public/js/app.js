d1f8e31997524c8ce3d12e9d540addc4e276e802
Jeremy Miller <jeremy@dischan.org>
2015-01-21 16:35:53 -0400

    Added some framework. Not sure I'll keep all of it yet.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,89 @@
+var APIURL = '/api/:resource/:action';
+
 var app = angular.module('ballistic', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute',
   'ngAnimate',
-]);
+]).constant('USER_ROLES', {
+  all: '*',
+  admin: 'admin',
+  creator: 'creator',
+  user: 'user'
+}).factory('API', function($resource) {
+  return $resource(APIURL);
+}).service('Session', function() {
+  this.create = function(sessionID, userID, userRole) {
+    this.id = sessionID;
+    this.userID = userID;
+    return this.userRole = userRole;
+  };
+  this.destroy = function() {
+    this.id = null;
+    this.userID = null;
+    return this.userRole = null;
+  };
+  return this;
+}).factory('authService', function($cookies, Session, API) {
+  var authService;
+  authService = {};
+  authService.createSession = function(sessionID, userID, userRole, rememberMe) {
+    $cookies.loggedIn = true;
+    return Session.create(sessionID, userID, userRole);
+  };
+  authService.destroySession = function() {
+    $cookies.loggedIn = false;
+    return Session.destroy();
+  };
+  authService.loadSession = function(callback) {
+    return API.get({
+      resource: 'auth',
+      action: 'session'
+    }, function(response, err) {
+      if (response.success) {
+        $cookies.loggedIn = true;
+        Session.create(response.data.session.id, response.data.user.id, response.data.user.role);
+        return callback(response.data.user);
+      } else {
+        return callback(false);
+      }
+    });
+  };
+  authService.isAuthenticated = function() {
+    console.log(Session.userID);
+    console.log('checking authentication');
+    return !!Session.userID;
+  };
+  authService.isAuthorized = function(authorizedRoles) {
+    if (!angular.isArray(authorizedRoles)) {
+      authorizedRoles = [authorizedRoles];
+      console.log(Session);
+    }
+    return authService.isAuthenticated() && authorizedRoles.indexOf(Session.userRole) !== -1;
+  };
+  return authService;
+}).run(function($rootScope, API, authService) {
+  API.get({
+    resource: 'auth',
+    action: 'getcsrftoken'
+  }, function(response, err) {
+    var csrfToken;
+    csrfToken = response.data.csrfToken;
+    console.log(csrfToken);
+    $rootScope.csrfToken = csrfToken;
+    return console.log($rootScope.csrfToken);
+  });
+  return $rootScope.$on('$locationChangeStart', function(event) {
+    return API.get({
+      resource: 'auth',
+      action: 'getcsrftoken'
+    }, function(response, err) {
+      var csrfToken;
+      csrfToken = response.data.csrfToken;
+      console.log(csrfToken);
+      $rootScope.csrfToken = csrfToken;
+      return console.log($rootScope.csrfToken);
+    });
+  });
+});
