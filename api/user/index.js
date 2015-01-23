b0b349299e0fa3a8aa20a3c2b6ab290460ba3526
Jeremy Miller <jeremy@dischan.org>
2015-01-23 11:46:55 -0400

    Added express session and api route. Started on middleware to load user.

---
+++ api/user/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./user');
+
+var router = express.Router();
+
+//router.get('/:id', controller.getInfo);
+
+module.exports = router;
--- app.js
+++ app.js
@@ -4,8 +4,25 @@ var express = require('express')
 var models = require('./models')
 var debug = require('debug')('ballistic');
 var app = express()
+var session = require('express-session')
 
 app.use(express.static('public'));
+app.use(session({
+  secret: 'tempsecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use ('/api', function (req, res, next) {
+  //req.models.session.find({id: req.session.id}, function (err, sessions) {
+    // if(!err && sessions.length > 0){
+    // } else {
+    //   next();
+    // }
+  //});
+});
+
+app.use('/api/users', require('./api/user'));
 
 app.route('/*').get(function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
