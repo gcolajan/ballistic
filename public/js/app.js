da775637efcb0a4f93c87f6277fcc18789e93c93
Jeremy Miller <jeremy@dischan.org>
2015-01-28 16:46:13 -0400

    Work on authentication.

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,26 @@ var app = angular.module('ballistic', [
   'ngAnimate',
 ]).factory('API', function($resource) {
   return $resource(APIURL);
+}).constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+}).constant('USER_ROLES', {
+  all: '*',
+  admin: 'user'
+}).run(function ($rootScope) {
+  //Authentication stuff
+  // $rootScope.$on('$routeChangeStart', function (event, next) {
+  //   var authorizedRoles = next.data.authorizedRoles;
+  //   if (!AuthService.isAuthorized(authorizedRoles)) {
+  //     event.preventDefault();
+  //     if (AuthService.isAuthenticated()) {
+  //       // user is not allowed
+  //       $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+  //     } else {
+  //       // user is not logged in
+  //       $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+  //     }
+  //   }
+  // });
+
+  // $rootScope.$on AUTH_EVENTS.notAuthenticated, showLogin
 })
