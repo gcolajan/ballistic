ae73e466fc04176227de32aed1cbe97437794f4a
Jeremy Miller <jeremy@dischan.org>
2015-02-05 14:15:57 -0400

    Work on transactions page.

--- public/js/routes.js
+++ public/js/routes.js
@@ -29,6 +29,12 @@ angular.module('ballistic').config(function($routeProvider, $locationProvider, U
     data: {
       authorizedRoles: [USER_ROLES.user]
     }
+  }).when('/account/:id/transactions', {
+    templateUrl: 'views/transactions.html',
+    controller: 'TransactionsCtrl',
+    data: {
+      authorizedRoles: [USER_ROLES.user]
+    }
   }).when('/404', {
     templateUrl: 'views/login.html',
     data: {
