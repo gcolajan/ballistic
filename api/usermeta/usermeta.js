101bbdcf8c6d597c6dc90541a6caf3a403b928f5
Jeremy Miller <jeremy@dischan.org>
2015-03-27 15:20:46 -0300

    Add server side validations and explanation of new settings to settings page.

--- api/usermeta/usermeta.js
+++ api/usermeta/usermeta.js
@@ -3,22 +3,35 @@ var debug = require('debug')('ballistic');
 var validator = require('validator');
 
 exports.update = function(req, res) {
-  if(!req.body.currency || !req.body.goal || !req.body.age){
+  if(!req.body.currency || !req.body.goal || !req.body.age || (req.body.includeInflation && !req.body.inflation) || (req.body.depletingPrincipal && !req.body.lifeSpan)){
     res.send({success: false, error: 'fields left empty'});
   } else if (!validator.isFloat(req.body.goal)) {
     res.send({success: false, error: 'goal must be a number'});
   } else if (!validator.isNumeric(req.body.age)) {
     res.send({success: false, error: 'age must be a number'});
+  } else if (req.body.includeInflation && !validator.isFloat(req.body.inflation)) {
+    res.send({success: false, error: 'inflation must be a number'});
+  } else if (req.body.depletingPrincipal && !validator.isNumeric(req.body.lifeSpan)) {
+    res.send({success: false, error: 'life span must be a number'});
+  } else if (req.body.depletingPrincipal && req.body.lifeSpan < req.body.age) {
+    res.send({success: false, error: 'life span must be greater than age'});
   } else {
-    req.user.getUserMetum().then(function(usermeta){
-      if(usermeta){
+    req.user.getUserMetum().then(function(usermeta) {
+      if (usermeta) {
         usermeta.currency = req.body.currency;
         usermeta.goal = req.body.goal;
         usermeta.age = req.body.age;
         usermeta.includeInflation = req.body.includeInflation;
-        usermeta.inflation = req.body.inflation;
+        
+        if (req.body.includeInflation) {
+          usermeta.inflation = req.body.inflation;
+        }
+        
         usermeta.depletingPrincipal = req.body.depletingPrincipal;
-        usermeta.lifeSpan = req.body.lifeSpan;
+        if (req.body.depletingPrincipal) {
+          usermeta.lifeSpan = req.body.lifeSpan;
+        }
+
         usermeta.save().then(function (){
           res.send({success: true, usermeta: usermeta});
         });
